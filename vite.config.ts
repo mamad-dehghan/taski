b4bf154a006f1b1a2c0dbc48f2b7c5c6395a0b18
mamad dehghan <101600675+mamad-dehghan@users.noreply.github.com>
2023-05-25 23:14:51 +0330

    add pwa to app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import {VitePWA} from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: false
+            },
+            includeAssets: [],
+            manifest: {
+                name: 'Taski todo manager',
+                short_name: 'Taski',
+                description: 'Task and event manager',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'logo/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'logo/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ],
+                start_url: "/"
+            }
+        }),
+        react()
+    ]
 })
