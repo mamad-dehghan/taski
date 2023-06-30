f2a6a016e0cefc621d280c66d21e8d93ce91cc7f
mamad dehghan <dehghanseyedmohammad@gmail.com>
2023-06-30 18:42:22 +0330

    add divider component for UI

---
+++ src/components/UI/divider/divider.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './divider.scss'
+
+type props = {}
+
+export const Divider = ({}: props) => {
+    return (
+        <div className="divider-wrapper">
+        <div className="divider full-width"/>
+        </div>
+    );
+};
