76b2df869b01e0c8322504a2c1cb257ba94493e9
mamad dehghan <dehghanseyedmohammad@gmail.com>
2023-06-30 18:40:59 +0330

    add sidebar item component for UI

---
+++ src/components/UI/sidebarItem/sidebarItem.tsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+import './sidebarItem.scss'
+import {Icon} from "@phosphor-icons/react";
+import {Badge} from "../badge/badge";
+
+type props = {
+    name: string,
+    Icon:Icon
+}
+
+export const SidebarItem = ({name, Icon}: props) => {
+    const [isActive, setIsActive] = useState<boolean>(false)
+    return (
+        <a tabIndex={0} className={`sidebar-item open ${isActive?"active":""}`}
+        onClick={()=>setIsActive(prevState => !prevState)}
+        >
+            <Badge show={true} display={"number"} value={4}>
+            <Icon size={24} weight={isActive ? 'fill' : 'regular'}/>
+            </Badge>
+            <p className="sidebar-item--label">{name}</p>
+        </a>
+    )
+}
